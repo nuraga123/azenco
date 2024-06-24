5c8397a971f03e6d6647b72b520d197d33d362cb
nuraga123 <sabrabat54@gmail.com>
2024-06-24 16:46:37 +0400

    fix form string page

--- pages/form__string/index.tsx
+++ pages/form__string/index.tsx
@@ -1,8 +1,9 @@
 import { postImportProductsFx } from '@/app/api/products'
-//import CopyElement from '@/components/elements/CopyElement'
+// import CopyElement from '@/components/elements/CopyElement'
 import Layout from '@/components/layout/Layout'
 import { getLocalStorageUser } from '@/localStorageUser'
 import { ChangeEvent, FormEvent, useState } from 'react'
+import styles from '@/styles/form__string/index.module.scss'
 
 function FormString() {
   const admin = getLocalStorageUser().usernameStorage
@@ -23,18 +24,18 @@ function FormString() {
       // Исправленная функция парсинга данных
       const items = lines?.map((line) => {
         const parts = line.split('\t')
-        const name = parts[0]
-        const azencoCode = parts[1]
+        const azencoCode = parts[0]
+        const name = parts[1]
         const price = parseFloat(parts[2]?.replace(',', '.'))
-        const type = parts[3] || 'digər' // По умолчанию 'digər', если тип не указан
-        const unit = parts[4]
+        const unit = parts[3]
+        const type = parts[4] || 'digər' // По умолчанию 'digər', если тип не указан
 
         return {
-          name,
           azencoCode,
+          name,
           price,
-          type,
           unit,
+          type,
           img: '',
         }
       })
@@ -44,20 +45,11 @@ function FormString() {
     const formattedString = parseDataToJSON(dataString)
     setResultString(formattedString)
   }
+
   console.log(resultString)
-  const handleCopyClick = async () => {
-    try {
-      debugger
-      await navigator.clipboard.writeText('ddd')
-      alert('Текст скопирован в буфер обмена')
-    } catch (err) {
-      alert('Не удалось скопировать текст')
-      console.log('Не удалось скопировать текст: ', err)
-    }
-  }
 
   const handleImport = async () => {
-    if (!resultString.length) alert('пустая строка')
+    if (!resultString.length) return alert('пустая строка')
     try {
       const result = await postImportProductsFx({ admin, str: resultString })
       alert('Import successful:')
@@ -69,26 +61,41 @@ function FormString() {
 
   return (
     <Layout title={'Kontaktlar'}>
-      <form onSubmit={handleSubmit}>
-        <h1>Format string</h1>
-        <textarea
-          value={dataString}
-          onChange={handleInputChange}
-          rows={10}
-          cols={50}
-        />
-        <button type="submit">Gönder</button>
-      </form>
-      {resultString && (
-        <div>
-          <h2>Результат:</h2>
-          <pre>{resultString}</pre>
-          <button onClick={handleCopyClick}>copy</button>
-        </div>
-      )}
+      <div className={styles['form-container']}>
+        <form onSubmit={handleSubmit}>
+          <h1>Format string</h1>
+          <h2>{`1) AzencoCode; 2) Materialin adi; 3) Qiymet; 4) Vahid; 5)Növü, 6)Şəkil linki`}</h2>
+          <h3>
+            {`Məsələn: 199888 qayka M 90 10.77 ədəd qayka 
+            https://нордком-карелия.рф/img/price/400_400/gajka_m_90_gost_1060594.jpg`}
+          </h3>
+          <br />
 
-      <div>
-        <button onClick={handleImport}>установить продукты</button>
+          <p>
+            {`azencoCode	materialın adı	Qiymet	Vahid	növü
+M00011278	3200x12000x2800 iki otaqlı dəhlizli konteyner	10771,57	ədəd	Konteyner`}
+          </p>
+
+          <textarea
+            value={dataString}
+            onChange={handleInputChange}
+            rows={10}
+            cols={50}
+          />
+          <button type="submit">Gönder</button>
+        </form>
+
+        {resultString && (
+          <div className={styles['result-container']}>
+            <h2>Nəticə:</h2>
+
+            <textarea value={resultString} className={styles.textarea} />
+          </div>
+        )}
+
+        <div className={styles['import-button']}>
+          <button onClick={handleImport}>məhsulları quraşdırın</button>
+        </div>
       </div>
     </Layout>
   )
