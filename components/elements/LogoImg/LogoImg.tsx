d153516a1178d9ec0b028ef5464e0b4dee0f2858
nuraga123 <sabrabat54@gmail.com>
2024-09-11 14:31:45 +0400

    fix

--- components/elements/LogoImg/LogoImg.tsx
+++ components/elements/LogoImg/LogoImg.tsx
@@ -1,12 +1,16 @@
+/* eslint-disable max-len */
 /* eslint-disable @next/next/no-img-element */
 import React from 'react'
 
-const LogoImg = () => (
-  <img
-    style={{ width: 25, height: 25, borderRadius: 10 }}
-    src="/icon/logo.png"
-    alt="logo"
-  />
-)
+const LogoImg = () => {
+  const urlImage = `https://vk.com/doc751126638_679936903?hash=GiV7zw4tSszHvwhf7Vfn4ucA6F8G8z0AeBdsOKkZaZg&dl=rijGHVYel0NdNXdzZqApDMGKd9OaxplEbakb4td2vvD`
+  return (
+    <img
+      style={{ width: 25, height: 25, borderRadius: 10 }}
+      src={urlImage}
+      alt="logo"
+    />
+  )
+}
 
 export default LogoImg
