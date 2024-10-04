d08032c5aa735c87a26d904168a42c9e0bb2ac94
nuraga123 <sabrabat54@gmail.com>
2024-10-04 16:50:06 +0400

    fix

--- pages/404.tsx
+++ pages/404.tsx
@@ -2,7 +2,7 @@ import { getWorkingServerFx } from '@/app/api/auth'
 import { useRouter } from 'next/router'
 import { useEffect, useState } from 'react'
 
-const Error404Page = () => {
+const Error404 = () => {
   const router = useRouter()
 
   const [serverStatus, setServerStatus] = useState<
@@ -19,34 +19,54 @@ const Error404Page = () => {
         setTimeout(() => {
           if (response) {
             setServerStatus('running')
-            router.push('/my')
+            router.push('/my') // Перенаправляем на страницу /my
           } else {
             setServerStatus('offline')
-            router.push('404')
           }
         }, 5000)
       } catch (error) {
         console.log('Error:', error)
-        setTimeout(() => setServerStatus('offline'), 1000)
+        setServerStatus('offline')
       }
     }
 
-    const intervalId = setInterval(getServerStatus, 10000)
+    getServerStatus() // Вызываем функцию один раз при загрузке
+    const intervalId = setInterval(getServerStatus, 10000) // Периодическая проверка статуса
     return () => clearInterval(intervalId)
-  }, [router, serverStatus])
+  }, [router])
 
   return (
     <div className="flex-container">
       <div className="text-center">
-        <button onClick={() => router.push('/help')}>proqramçı yardımı</button>
-        <h2>Server işləmir</h2>
-        <br />
-        <h2 className="fadeIn">Xahiş edirik, daha sonra yenidən cəhd edin</h2>
-        <br />
-        <button onClick={() => router.push(`/my`)}>menyuya qayıdın</button>
+        {serverStatus === 'offline' && (
+          <>
+            <h2>Server işləmir</h2>
+            <h2 className="fadeIn">
+              Xahiş edirik, daha sonra yenidən cəhd edin
+            </h2>
+            <button onClick={() => router.push('/help')}>
+              proqramçı yardımı
+            </button>
+            <br />
+            <button onClick={() => router.push('/my')}>menyuya qayıdın</button>
+          </>
+        )}
+        {serverStatus === 'loading' && (
+          <>
+            <h2>Server işləmir</h2>
+            <h2 className="fadeIn">
+              Xahiş edirik, daha sonra yenidən cəhd edin
+            </h2>
+            <button onClick={() => router.push('/help')}>
+              proqramçı yardımı
+            </button>
+            <br />
+            <button onClick={() => router.push('/my')}>menyuya qayıdın</button>
+          </>
+        )}
       </div>
     </div>
   )
 }
 
-export default Error404Page
+export default Error404
